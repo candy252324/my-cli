ce47ad752b793d521f23ebad2d68b073ac8cc67d
cjh <jiahuan.chen@xiaobao100.com>
2020-03-26 16:35:11 +0800

    支持浏览器自动刷新

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin")
 const TerserPlugin = require('terser-webpack-plugin');   // https://www.npmjs.com/package/terser-webpack-plugin
 
 module.exports = {
@@ -36,6 +37,24 @@ module.exports = {
   //     }),
   //   ],
   // },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",    // 生成的html文件名称
+      template: "./src/index.html",     // 以当前目录下的的 index.html 为模板来生成html
+      inject: "head",   // script标签插入到 html中的位置，head/body/false, 默认为插入body, 若设为false,则生成的html中不会自动插入script标签
+      chunks: ["index"], // 引入打包后的index.js
+      minify: {
+        removeComments: true,     // 删除 html 中的注释
+        collapseWhitespace: true,   // 删除 html 中空格
+      }
+    })
+  ],
+  devServer: {
+    port: "9000",
+    contentBase: path.join(__dirname, './dist'),
+    inline: false,  // false:iframe 模式，在网页中嵌入了一个iframe。 true:inline模式,浏览器不能自动刷新?暂时不知何故
+    open: true, // 自动打开浏览器
+  },
   module: {
     rules: [
       {
